e6425f17b0e252b348c17874f1147d6ff40f7bad
alexander matveev <dev.alexander.matveev@gmail.com>
2026-04-16 23:06:16 +0200

    feat: spring boot scaffold with web + websocket dependencies

---
+++ src/main/java/com/jacuum/JacuumApplication.java
@@ -0,0 +1,11 @@
+package com.jacuum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacuumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JacuumApplication.class, args);
+    }
+}
